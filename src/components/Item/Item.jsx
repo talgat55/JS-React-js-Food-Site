bddab05823a07fc100effc9f067e53250e5ef038
talgat55 <lightx_design@yahoo.com>
2020-06-18 01:13:12 +0600

    end with item product

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,24 +1,68 @@
 import React from 'react';
 import NoImage from '../../static/noimage.png';
-
+import basketImg from '../../static/bug.svg';
+import styled  from 'styled-components';
 const Item = ({name, price}) => {
 
     return (
-        <li className="product-item">
-            <div className="product-item__image">
-                <img
-                    src={NoImage}
-                    alt="Product item"
-                />
+        <ItemComponent className="product-item">
+            <div className="product-item__wrapper">
+                <div className="product-item__image">
+                    <img
+                        src={NoImage}
+                        alt="Product item"
+                    />
+                </div>
+                <div  className="product-item__content">
+                    <h3 className="product-item__title">
+                        {name}
+                    </h3>
+                    <div className="product-item__bottom-content">
+                        <div className="product-item__price">
+                            $ {price}
+                        </div>
+                        <a href="#" className="add-to-cart">
+                            <img
+                                src={basketImg}
+                                alt="Icon"
+                                />
+                        </a>
+                    </div>
+                </div>
             </div>
-            <h3 className="product-item__title">
-                {name}
-            </h3>
-            <div className="product-item__price">
-                {price}
-            </div>
-        </li>
+        </ItemComponent>
     )
 };
+const ItemComponent = styled.li`
+    .product-item__wrapper{
+        box-shadow: 0px 6px 35px rgba(155, 166, 178, 0.25);
+        margin: 0 10px 40px 10px;
+        border-radius: 15px;
+        overflow: hidden;
+        .product-item__image{
+            img{
+                max-width: 100%;
+            }
+        }
+        .product-item__content{
+            padding: 15px;
+            .product-item__bottom-content{
+                margin-top: 15px;
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                .add-to-cart img  {
+                    width: 25px;
+                    height: 25px;
+                }
+            }
+            .product-item__title{
+                font-size: 20px;
+                line-height: 24px;
+            }
+        }
+    }
+`;
+
 
 export default Item;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,7 @@
 import React,{useState, useEffect} from 'react';
 import {getProducts} from "../api/products";
 import Item from "../components/Item/Item";
-
+import styled  from 'styled-components';
 const Products = () => {
     const [data, setData] = useState([]);
 
@@ -13,18 +13,41 @@ const Products = () => {
 
 
     return (
-        <>
-            <ul>
-            {data.map((item, index) => (
-                <Item
-                    key={index}
-                    name={item.name}
-                    price={item.price}
-                />
-            ))}
-            </ul>
-        </>
+        <ProductComponent>
+            <div className="container">
+                <ul>
+                    {data.map((item, index) => (
+                        <Item
+                            key={index}
+                            name={item.name}
+                            price={item.price}
+                        />
+                    ))}
+                </ul>
+            </div>
+        </ProductComponent>
     )
 };
-
+const ProductComponent = styled.section`
+    margin-top: 50px;
+    .container{
+        width: 1140px;
+        margin: 0 auto;
+    }
+    ul{
+        margin: 0;
+        padding: 0;
+        display: -ms-flexbox;
+        display: flex;
+        -ms-flex-wrap: wrap;
+        flex-wrap: wrap;
+        li{
+            list-style: none;
+            width: 33.333%;
+            padding: 0 10px 10px 10px;
+        }
+    }
+       
+    
+`;
 export default Products;
